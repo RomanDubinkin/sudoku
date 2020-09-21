f198292585bf7f60a5b6fd3d91d07d1efee8c4cf
Roman <rdubinkin@gmail.com>
2020-09-21 19:16:59 -0400

    Release 1: still no graphics

--- script.js
+++ script.js
@@ -2,6 +2,27 @@ const { indexFinder } = require('./index-finder.js');
 const { checkArray } = require('./checkArray.js');
 const { updateArray } = require('./updateArray.js');
 
+function findPossible(index, sudokuArray) {
+  let isPossible = new Array(10).fill(true);
+  isPossible[0] = false;
+  for (let j = 0; j < 3; j += 1) {
+    let indices = [...indexFinder(index, j)];
+    updateArray(indices, sudokuArray, isPossible);
+  }
+  return isPossible;
+}
+
+function findMatch(isPossible, index, pos, sudokuArray) {
+  let row = [...isPossible];
+  let indices = [...indexFinder(index, pos)].filter(el => sudokuArray[el] === '-');
+  indices.forEach((el) => {
+    let isPossible = findPossible(el, sudokuArray);
+    let indices = checkArray(isPossible, 1);
+    indices.forEach((el) => row[el] = false);
+  });
+  return row;
+}
+
 function sudokuSolver(string) {
   const sudokuArray = string.split('');
   let toContinue;
@@ -9,12 +30,13 @@ function sudokuSolver(string) {
     toContinue = false;
     for (let i = 0; i < 81; i += 1) {
       if (sudokuArray[i] === '-') {
-        let isPossible = new Array(10).fill(true);
-        isPossible[0] = false;
-        for (let j = 0; j < 3; j += 1) {
-          let indices = [...indexFinder(i, j)];
-          updateArray(indices, sudokuArray, isPossible);
-        }
+        // let isPossible = new Array(10).fill(true);
+        // isPossible[0] = false;
+        // for (let j = 0; j < 3; j += 1) {
+        //   let indices = [...indexFinder(i, j)];
+        //   updateArray(indices, sudokuArray, isPossible);
+        // }
+        let isPossible = findPossible(i, sudokuArray);
         if (isPossible[checkArray(isPossible)]) {
           sudokuArray[i] = checkArray(isPossible);
           toContinue = true;
@@ -22,8 +44,26 @@ function sudokuSolver(string) {
       }
     }
   } while (toContinue === true);
+
+  do {
+    toContinue = false;
+    for (let i = 0; i < 81; i += 1) {
+      if (sudokuArray[i] === '-') {
+        let isPossible = findPossible(i, sudokuArray);
+        for (let j = 0; j < 3; j += 1) {
+          let match = findMatch(isPossible, i, j, sudokuArray);
+          if (isPossible[checkArray(match)]) {
+            sudokuArray[i] = checkArray(match);
+            toContinue = true;
+            break;
+          }
+        }
+      }
+    }
+  } while (toContinue === true);
+
   return sudokuArray.join(' ');
 }
 
-const string = '1-58-2----9--764-52--4--819-19--73-6762-83-9-----61-5---76---3-43--2-5-16--3-89--';
+const string = '3-26-9--55--73----------9-----94----------1-9----57-6---85----6--------3-19-82-4-';
 console.log(sudokuSolver(string));
