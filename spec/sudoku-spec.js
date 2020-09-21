8c8f3366ffc4af8b8cc507140a2e00e90a03eafe
Anton <anton.sipin@gmail.com>
2020-09-21 15:59:31 +0300

    Added function updateArray + Jasmine

--- spec/sudoku-spec.js
+++ spec/sudoku-spec.js
@@ -1,2 +1,15 @@
-const sudoku = require('../sudoku.js')
+describe('Function updateArray check', () => {
+  describe('Check first index', () => {
+    it('function updateArray', () => {
+      expect(updateArray([0])).toEqual([false, true, false, false, false, true, false, false, false, false]);
+    });
+  });
+});
 
+describe('Function updateArray check', () => {
+  describe('Check last index', () => {
+    it('function updateArray', () => {
+      expect(updateArray([9])).toEqual([true, true, false, false, false, true, false, false, false, false]);
+    });
+  });
+});
