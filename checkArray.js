573b711c79de1e99f8bac3137de05f6cc3a48855
gayazurna <gayazurna@gmail.com>
2020-09-21 16:14:12 +0300

    function checkArray

---
+++ checkArray.js
@@ -0,0 +1,17 @@
+// function returns index of true's element
+function checkArray(isPossible) {
+  let indices = [];
+  let element = true;
+  let idx = isPossible.indexOf(element);
+  while (idx != -1) {
+    indices.push(idx);
+    idx = isPossible.indexOf(element, idx + 1);
+  }
+  if (indices.length > 1 && indices.length < 1) {
+    return -1;
+  }
+  return indices;
+}
+module.exports = {
+  checkArray,
+};
